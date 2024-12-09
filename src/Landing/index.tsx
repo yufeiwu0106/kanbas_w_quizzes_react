4a0324d7709cc556177b08b98553d4232df9523e
Gray Wu <graywu@Weirans-MacBook-Air.local>
2024-12-08 16:52:46 -0800

    fix landing page

--- src/Landing/index.tsx
+++ src/Landing/index.tsx
@@ -11,31 +11,28 @@ export default function Landing() {
     <div>
       {/* Full name and section number */}
       <div style={{ marginBottom: "20px" }}>
-        <h1>Yufei Wu - CS5610 - Section 1</h1>
+        <h1>Group Project - Di He, Kaichen Qu, Liuyi Yang, Yufei Wu </h1>
       </div>
 
       {/* Heading for GitHub link */}
-      <h1>Repositories Link</h1>
+      <h1>Quizzes Project Repositories Link</h1>
       <div style={{ marginTop: "20px" }}>
         <a
-          href="https://github.com/yufeiwu0106/kanbas-react-web-app/tree/a2"
+          href="https://github.com/yufeiwu0106/kanbas_w_quizzes_node"
           target="_blank"
           rel="noopener noreferrer"
         >
-          All Source Code Repositories on GitHub
+          Github: Node server kanbas_w_quizzes_node
+        </a>
+        <br />
+        <a
+          href="https://github.com/yufeiwu0106/kanbas_w_quizzes_react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github: React UI kanbas_w_quizzes_react
         </a>
       </div>
-
-      <h1>Labs</h1>
-
-      <ul>
-        <li><Link to="/Labs/Lab1">Labs</Link></li>
-        <li><Link to="/Labs/Lab1">Lab 1</Link></li>
-        <li><Link to="/Labs/Lab2">Lab 2</Link></li>
-        <li><Link to="/Labs/Lab3">Lab 3</Link></li>
-        <li><Link to="/Labs/Lab4">Lab 4</Link></li>
-        <li><Link to="/Labs/Lab5">Lab 5</Link></li>
-      </ul>
 
       <h1>Kanbas</h1>
 
